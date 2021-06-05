d9d33ee79c290c31b1f3e7196c2418f79b4a75fd
hritikchokker <hchoker7@mail.com>
2021-06-05 17:24:39 +0530

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">Welcome to Vue</h1>
+
+        <h3 class="subheading font-weight-regular">
+          Vue app with vuerouter, vuex, vuetify, unit test,e2e, pwa
+        </h3>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "HelloWorld",
+
+  data: () => ({}),
+});
+</script>
