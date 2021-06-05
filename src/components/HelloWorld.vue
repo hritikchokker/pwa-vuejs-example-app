d411322ee2a334f8fdc39677302185fafccc754d
hritikchokker <hchoker7@mail.com>
2021-06-06 02:43:43 +0530

    minor changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -29,3 +29,5 @@ export default Vue.extend({
   data: () => ({}),
 });
 </script>
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,38 @@
 <template>
-  <hello-world />
+  <div class="container">
+    <h1>{{ dynamicMessage }} Home component</h1>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "../components/HelloWorld.vue";
+// import HelloWorld from "../components/HelloWorld.vue";
 
 export default Vue.extend({
   name: "Home",
-
+  data: () => {
+    return {
+      dynamicMessage: "Hello World from Vue",
+    };
+  },
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 });
 </script>
+<style scoped lang="scss">
+.container {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  margin-top: 50vh;
+  h1 {
+    text-align: center;
+    color: aqua;
+  }
+  h1:hover {
+    transform: scale(1.1);
+    transition: 0.5s all ease-in-out;
+  }
+}
+</style>
